25e19d54290854a6b7c23e587ffeb924e56251d8
Daniel Luong <danielluong@hotmail.ca>
2020-12-13 23:56:41 -0800

    prepping for front end work, getting ready to take user input for player_score modifiers

--- BackEnd/nhl_api.py
+++ BackEnd/nhl_api.py
@@ -5,7 +5,8 @@ import json
 class NhlScrape:
     def __init__(self):
         self.url = 'https://statsapi.web.nhl.com/'
-        self.rosters = {}
+        self.players = []
+        # TODO add multiplier for each statistic for the player_score
 
     def get_all_teams(self):
         # The api contains every team in existence, therefore some do not
@@ -20,34 +21,60 @@ class NhlScrape:
             if data.ok:
                 temp = {i: data.json()["roster"]}
                 rosters.update(temp)
+        print(rosters)
 
-        print(json.dumps(rosters, indent=4, sort_keys=True))
-
-    def get_team(self):
-        # TEST FUNCTION
-        roster = {}
-        data = requests.get(url=f"{self.url}api/v1/teams/1/roster")
-        roster.update({1: data.json()["roster"]})
-        print(json.dumps(roster, indent=4, sort_keys=True))
-
-        # modularize code here, needs to be own function
-        for team in roster:
-            for player in roster[team]:
+        # TODO modularize code here, should be own function
+        for team in rosters:
+            for player in rosters[team]:
                 name = player["person"]["fullName"]
                 position = player["position"]["code"]
+                player_endpoint = player["person"]["link"]
                 if not position == "G":
-                    print(name)
-                    print(position)
-                    self.get_stats(player["person"]["link"])
+                    stats = self.get_stats(player_endpoint)
+                    temp = {"name": name}
+                    temp.update(stats)
+                    self.calculate_player_score(stats)
+                    self.players.append(temp)
+                    print(temp)
 
     def get_stats(self, player_endpoint: str):
         endpoint = "/stats?stats=statsSingleSeason&season=20192020"
         data = requests.get(url=f"{self.url}{player_endpoint}{endpoint}")
-        stats = data.json()['stats'][0]["splits"][0]['stat']
-        print(stats)
+        splits = data.json()['stats'][0]['splits']
+        if splits:
+            stats = splits[0]['stat']
+        else:
+            stats = []
+        return stats
 
+    def calculate_player_score(self, stats):
+        player_score = 'N/A'
+        if stats:
+            games = stats['games']
+            for statistic in stats:
+                if not ('time' in statistic.lower() or
+                        'pct' in statistic.lower()):
+                    print(statistic, stats[statistic])
+        return player_score
+    # stats to consider:
+    # games --> have a minimum games played
+    # assists
+    # goals
+    # pim
+    # shots
+    # hits
+    # powerPlayGoals
+    # powerPlayPoints
+    # penaltyMinutes
+    # gameWinningGoals
+    # overTimeGoals --> may have a problem with this one for time
+    # shortHandedGoals
+    # blocked
+    # plusMinus
+    # points
+    # shifts
 
 if __name__ == '__main__':
     test = NhlScrape()
-    # test.get_all_teams()
-    test.get_team()
+    test.get_all_teams()
+    # test.get_team()
--- gui.py
+++ gui.py
@@ -1,12 +1,47 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text("Hello from test")], [sg.Button("OK")]]
+# stats to consider:
+# games
+# assists
+# goals
+# pim
+# shots
+# hits
+# powerPlayGoals
+# powerPlayPoints
+# penaltyMinutes
+# gameWinningGoals
+# overTimeGoals
+# shortHandedGoals
+# blocked
+# plusMinus
+# points
+# shifts
+
+stats = ['games', 'assists', 'goals', 'pim', 'shots', 'hits',
+         'powerPlayGoals', 'powerPlayPoints', 'penaltyMinutes',
+         'gameWinningGoals', 'overTimeGoals', 'shortHandedGoals', 'blocked',
+         'plusMinus', 'points', 'shifts']
+categories = []
+
+# for stat in stats:
+#     categories.append(sg.Text(stat))
+#     categories.append(sg.Input(key=stat))
+categories.append(sg.Text(stats[0]))
+categories.append(sg.Input(key=stats[0]))
+categories.append(sg.Text(stats[1]))
+categories.append(sg.Input(key=stats[1]))
+
+
+layout = [categories, [sg.Button("test")]]
 
 window = sg.Window("Demo", layout)
 
 while True:
     event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+    print(values)
+    print(event)
+    if event == sg.WIN_CLOSED:
         break
 
 window.close()
