caea60cacb36c477d6f387dc8e587b37cd1e1ed3
Daniel Luong <danielluong@hotmail.ca>
2020-12-14 12:48:37 -0800

    completed integration with gui and NHL Scraper, updated multipliers

---
+++ FrontEnd/nhl_api.py
@@ -0,0 +1,124 @@
+import requests
+import json
+
+
+# stats to consider:
+# games --> have a minimum games played
+# assists
+# goals
+# pim
+# shots
+# hits
+# powerPlayGoals
+# powerPlayPoints
+# gameWinningGoals
+# overTimeGoals --> may have a problem with this one for time
+# shortHandedGoals
+# blocked
+# plusMinus
+# points
+# shifts
+
+class NhlScrape:
+    def __init__(self, **kwargs):
+        self.url = 'https://statsapi.web.nhl.com/'
+        self.players = []
+        self.multipliers = None
+        self.games_played = None
+        self.output_file = None
+        self.per_game = None
+
+    def set_scraper(self, **kwargs):
+        self.multipliers = kwargs
+        self.games_played = int(kwargs['games_played'])
+        self.output_file = kwargs['output']
+        self.per_game = int(kwargs['per_game'])
+        del self.multipliers['games_played']
+        del self.multipliers['output']
+        del self.multipliers['per_game']
+
+    def get_rosters(self):
+        # The api contains every team in existence, therefore some do not
+        # have roster, need to get every team and sort which one's have
+        # active rosters, num_of_teams will eventually need to change
+        num_of_teams = 55
+        rosters = {}
+
+        for i in range(1, num_of_teams + 1):
+            endpoint = f"api/v1/teams/{i}/roster"
+            data = requests.get(url=f"{self.url}{endpoint}")
+            if data.ok:
+                temp = {i: data.json()["roster"]}
+                rosters.update(temp)
+        print(rosters)
+
+        # TODO modularize code here, should be own function
+        for team in rosters:
+            for player in rosters[team]:
+                name = player["person"]["fullName"]
+                position = player["position"]["code"]
+                player_endpoint = player["person"]["link"]
+                if not position == "G":
+                    stats = self.get_stats(player_endpoint)
+                    player_score = self.calculate_player_score(stats)
+                    temp = {"name": name, "position": position}
+                    temp.update(stats)
+                    temp.update({"player_score": player_score})
+                    self.players.append(temp)
+                    print(temp)
+        print(self.players)
+
+    def get_stats(self, player_endpoint: str):
+        endpoint = "/stats?stats=statsSingleSeason&season=20192020"
+        data = requests.get(url=f"{self.url}{player_endpoint}{endpoint}")
+        splits = data.json()['stats'][0]['splits']
+        if splits:
+            stats = splits[0]['stat']
+        else:
+            stats = []
+        return stats
+
+    def calculate_player_score(self, stats):
+        player_score = 0
+        if stats:
+            games = stats['games']
+            if games > self.games_played:
+                for value in self.multipliers:
+                    if self.per_game:
+                        stat = float(stats[value]) / games
+                    else:
+                        stat = float(stats[value])
+                    multiplier = float(self.multipliers[value])
+                    player_score += self.multiply_stat(stat, multiplier)
+        print(f"player score: {player_score}")
+        return player_score
+
+    def multiply_stat(self, stat, multiplier):
+        player_score = stat * multiplier
+        return player_score
+
+
+if __name__ == '__main__':
+    settings = {'games': '1',
+                'assists': '1',
+                'goals': '1',
+                'pim': '.5',
+                'shots': '.3',
+                'hits': '.2',
+                'powerPlayGoals': '0',
+                'powerPlayPoints': '1',
+                'gameWinningGoals': '0',
+                'overTimeGoals': '0',
+                'shortHandedGoals': '0',
+                'blocked': '.5',
+                'plusMinus': '1',
+                'points': '1',
+                'shifts': '0',
+                'games_played': '40',
+                'output': 'test',
+                'per_game': True
+                }
+
+    test = NhlScrape()
+    test.set_scraper(**settings)
+    test.get_rosters()
--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+from FrontEnd.nhl_api import NhlScrape
+
 
 # stats to consider:
 # games
@@ -19,26 +21,41 @@ import PySimpleGUI as sg
 # shifts
 
 stats = ['games', 'assists', 'goals', 'pim', 'shots', 'hits',
-         'powerPlayGoals', 'powerPlayPoints', 'penaltyMinutes',
+         'powerPlayGoals', 'powerPlayPoints',
          'gameWinningGoals', 'overTimeGoals', 'shortHandedGoals', 'blocked',
          'plusMinus', 'points', 'shifts']
-categories = []
+layout = [[sg.Text("Stat Multipliers")]]
 
-# for stat in stats:
-#     categories.append(sg.Text(stat))
-#     categories.append(sg.Input(key=stat))
-categories.append(sg.Text(stats[0]))
-categories.append(sg.Input(key=stats[0]))
-categories.append(sg.Text(stats[1]))
-categories.append(sg.Input(key=stats[1]))
+text_column = []
+input_column = []
+for stat in stats:
+    text_column.append([sg.Text(stat)])
+    input_column.append([sg.In(key=stat)])
 
+output_file_row = [sg.Text("Output File Name:   "), sg.In(key="output")]
+games_played_row = [sg.Text("Min Games Played: "), sg.In(key="games_played")]
+per_game_row = [sg.Checkbox(key="per_game", text="Per Game Mode")]
 
-layout = [categories, [sg.Button("test")]]
+layout.append([sg.Column(text_column), sg.Column(input_column)])
+layout.append([sg.Text("")])
+layout.append(games_played_row)
+layout.append(output_file_row)
+layout.append(per_game_row)
+layout.append([sg.Button("Submit")])
 
-window = sg.Window("Demo", layout)
+window = sg.Window("Fantasy Hockey Player Tool", layout)
+
+scraper = NhlScrape()
 
 while True:
     event, values = window.read()
+    try:
+        scraper.set_scraper(**values)
+        scraper.get_rosters()
+    except TypeError as e:
+        print(e)
+    except ValueError as e:
+        print("user value error")
     print(values)
     print(event)
     if event == sg.WIN_CLOSED:
